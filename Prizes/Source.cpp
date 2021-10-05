20149bf6c8f0d0128c61348ebf2448d693dad2b1
hubo00 <hub.bukowski@gmail.com>
2021-10-05 10:49:34 +0100

    Fixed Prize giveaway

--- Prizes/Source.cpp
+++ Prizes/Source.cpp
@@ -15,39 +15,32 @@ bool search(int array[], int lenArray, int toFind);
 
 int main() {
 
+	const int NUMPRIZES = 4;
 	const int NUMFINALISTS = 25;
-
 	int finalists[NUMFINALISTS];
+	int previousWinners[NUMPRIZES];
 
 	std::default_random_engine defEngine(time(0));
 	std::uniform_int_distribution<int> intDistro(1, 25);
 
-	// An Algorithm which populates the finalists array with the number of specified finalists
+	// An loop which populates the finalists array with the number of specified finalists
 	for (int finalistNum = 0; finalistNum < NUMFINALISTS; finalistNum++) {
-		int randNum = intDistro(defEngine);
-		// If finalist index is greater than 0, compare random number to other numbers in the array
-		if (finalistNum != 0) {
-			// Loop and reassign the random number until it is unique
-			while (search(finalists, finalistNum, randNum)) {
-				for (int i = 0; i < finalistNum; i++) {
-					if (randNum == finalists[i]) {
-						randNum = intDistro(defEngine);
-					}
-				}
-			}
-		}
-		finalists[finalistNum] = randNum;
-		std::cout << finalists[finalistNum] << std::endl;
+		int finalistNumber = finalistNum + 1;
+		finalists[finalistNum] = finalistNumber;
 	}
 
 	// Algorithm which loops through the number of prizes, for each prize it chooses a random winning number (1-25)
-	for (int prizeNum = 1; prizeNum <= 4; prizeNum++) {
+	for (int prizeNum = 0; prizeNum < NUMPRIZES; prizeNum++) {
 		int winner = intDistro(defEngine);
+		// Loop while the new winning value is in the previous winners array and assign a new random
+		while (search(previousWinners, NUMPRIZES, winner)) {
+			winner = intDistro(defEngine);
+		}
 		for (int finalistNum = 0; finalistNum < NUMFINALISTS; finalistNum++) { // Loops and compares winning number to 25 finalists
-			// If this finalist is a winner, print out a message and set their value to null, so as to avoid repeating winners
+			// If this finalist is a winner, print out a message and add the winning value to winner array so as to avoid repeating winners
 			if (finalists[finalistNum] == winner) {
-				std::cout << "Winner of prize " << prizeNum << " is finalist number " << finalists[finalistNum] << std::endl;
-				finalists[finalistNum] = NULL;
+				std::cout << "Winner of prize " << prizeNum+1 << " is finalist number " << finalists[finalistNum] << std::endl;
+				previousWinners[prizeNum] = winner;
 			}
 		}
 	}
